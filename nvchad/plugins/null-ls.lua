535b90169ae70b4c5bc9d00c3f44c1ead5736548
wuliuqii <h1090703848@gmail.com>
2022-08-29 22:17:44 +0800

    null-ls formatting on save

--- nvchad/plugins/null-ls.lua
+++ nvchad/plugins/null-ls.lua
@@ -17,9 +17,26 @@ local sources = {
 
 	-- rust
 	b.formatting.rustfmt,
+	b.formatting.taplo,
 }
 
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 null_ls.setup({
 	debug = true,
 	sources = sources,
+
+	-- formatting on save
+	on_attach = function(client, bufnr)
+		if client.supports_method("textDocument/formatting") then
+			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = augroup,
+				buffer = bufnr,
+				callback = function()
+					-- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+					vim.lsp.buf.formatting_sync()
+				end,
+			})
+		end
+	end,
 })
